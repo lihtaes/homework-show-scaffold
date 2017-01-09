0932a1043fcbf304e392c1dd87ae844b2b14f4c5
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-01-08 19:50:45 -0700

    ran db:migrate on last final test scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109015541) do
+ActiveRecord::Schema.define(version: 20170109020256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "final_tests", force: :cascade do |t|
+    t.string   "this"
+    t.string   "that"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_templates", force: :cascade do |t|
     t.string   "new_thing"
